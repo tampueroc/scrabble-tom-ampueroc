8de65071befc9578738719d9fee68b2ad3da21ee
tom-ampueroc <46233260+tom-ampueroc@users.noreply.github.com>
2021-07-28 18:23:40 -0400

    added methods to interface

--- src/main/java/cl/uchile/dcc/scrabble/visitor/IVisitor.java
+++ src/main/java/cl/uchile/dcc/scrabble/visitor/IVisitor.java
@@ -1,4 +1,8 @@
 package cl.uchile.dcc.scrabble.visitor;
 
+import cl.uchile.dcc.scrabble.types.ITypes;
+
 public class IVisitor {
+    ITypes visitIf(ASTif control);
+    void visitWhile(ASTwhile control);
 }
