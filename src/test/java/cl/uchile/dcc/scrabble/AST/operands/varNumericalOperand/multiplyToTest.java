47f1adb20b31ceae5801f6fce24a5cd64be41ffa
tom-ampueroc <46233260+tom-ampueroc@users.noreply.github.com>
2021-08-02 23:43:00 -0400

    Finished test for var operands

---
+++ src/test/java/cl/uchile/dcc/scrabble/AST/operands/varNumericalOperand/multiplyToTest.java
@@ -0,0 +1,47 @@
+package cl.uchile.dcc.scrabble.AST.operands.varNumericalOperand;
+
+import cl.uchile.dcc.scrabble.AST.varNode;
+import cl.uchile.dcc.scrabble.types.ITypes;
+import cl.uchile.dcc.scrabble.types.numbers.SFloat;
+import cl.uchile.dcc.scrabble.types.numbers.SInteger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Random;
+
+import static cl.uchile.dcc.scrabble.AST.varNode.getMapVar;
+import static org.junit.jupiter.api.Assertions.*;
+
+class multiplyToTest {
+    private Random rng;
+
+    @RepeatedTest(100)
+    void operate() {
+        Map<String, ITypes> map = getMapVar();
+        int random_1 = rng.nextInt();
+        double random_dbl = new Random().nextDouble();
+        double random_dbl_1 = random_1*random_dbl;
+
+        SFloat SFlt_1 = new SFloat(random_dbl_1);
+        SInteger SInt_1 = new SInteger(random_1);
+
+        varNode a = new varNode("a", SFlt_1);
+        varNode b = new varNode("b", SInt_1);
+        a.operate();
+        b.operate();
+
+        multiplyTo add_1 = new multiplyTo("a", "b");
+        double expectedValue_1 = SFlt_1.multiply(SInt_1).operate().asFloat().getValue();
+        SFloat expected_1 = new SFloat(expectedValue_1);
+        add_1.operate();
+        assertEquals(expected_1, map.get("a"));
+    }
+
+    @BeforeEach
+    void setUp() {
+        int seed = new Random().nextInt();
+        rng = new Random(seed);
+    }
+}
